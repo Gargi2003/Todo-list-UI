f81ea4472b6bbc362f0f444d8b9f57724e5b591d
Gargi2003 <gargibanerjee49@gmail.com>
2023-06-25 22:53:47 +0530

    modified work component

--- todo-list/src/app/your-work/your-work.component.ts
+++ todo-list/src/app/your-work/your-work.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class YourWorkComponent {
   ngOnInit() {
     this.getProjects();
+    this.getAudit();
   }
 
   getRandomColor() {
@@ -18,14 +19,16 @@ export class YourWorkComponent {
       '#FFD180'  // Light orange
     ];
     const index = Math.floor(Math.random() * colors.length);
+
     return colors[index];
   }
+
   project: any;
-  
 
-  constructor(private http: HttpClient,private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-  projectId:any
+  projectId: any;
+
   getProjects() {
     const httpOptions = {
       headers: new HttpHeaders({
@@ -37,7 +40,75 @@ export class YourWorkComponent {
       this.project = response;
     });
   }
+
   navigateToTasks(projectId: number) {
     this.router.navigate(['/tasks'], { queryParams: { projectId: projectId } });
   }
+
+  audit: any;
+  tasksByCreateDate: { createDate: string, label: string, tasks: any[] }[] = [];
+  issueType: any
+  getAudit() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      }),
+    };
+
+    this.http.get('http://localhost:8081/audit/list', httpOptions).subscribe(response => {
+      this.audit = response;
+      // Group tasks by create date
+      const groupedTasks: { [key: string]: any[] } = {};
+      for (const task of this.audit) {
+        const createDate = task.created_at.split('T')[0]; // Extract the date portion
+
+        // Check if the create date group already exists
+        if (!groupedTasks.hasOwnProperty(createDate)) {
+          groupedTasks[createDate] = [];
+        }
+
+        // Add the task to the respective create date group
+        groupedTasks[createDate].push(task);
+        this.getProjectById(task.project_id)
+        this.issueType = task.issue_type
+      }
+
+      // Convert the grouped tasks into the tasksByCreateDate array
+      for (const createDate in groupedTasks) {
+        if (groupedTasks.hasOwnProperty(createDate)) {
+          const label = this.getLabel(createDate);
+          this.tasksByCreateDate.push({ createDate: createDate, label: label, tasks: groupedTasks[createDate] });
+        }
+      }
+    });
+  }
+
+  key: any;
+  proj: any;
+  projname: any;
+
+  getProjectById(id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      }),
+    };
+    this.http.get('http://localhost:8081/projects/get?id=' + id, httpOptions).subscribe(response => {
+      this.proj = response;
+      this.key = this.proj.project_key;
+      this.projname = this.proj.name;
+    });
+  }
+
+  getLabel(date: any): string {
+    const createDate = new Date(date);
+    const createMonth = createDate.getMonth();
+
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return monthNames[createMonth];
+  }
+
+
 }
